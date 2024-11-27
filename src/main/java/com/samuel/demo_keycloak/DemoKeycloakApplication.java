c5b35b790197d5bfaa5814dac8bb1f05d90e095a
samuelgbenga <samuelgbenga972@gmail.com>
2024-11-27 13:09:28 +0100

    new originn main

---
+++ src/main/java/com/samuel/demo_keycloak/DemoKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.demo_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/samuel/demo_keycloak/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.samuel.demo_keycloak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults())  // Enable OAuth2 login
+                .build();
+    }
+
+}
